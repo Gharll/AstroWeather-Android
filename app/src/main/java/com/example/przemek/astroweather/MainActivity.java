e67ee1f094054b0d0b236160d87895aa2f24370e
Gharll <mrprzemob@gmail.com>
2018-04-06 15:29:17 +0200

    AstroWeather import library

--- app/src/main/java/com/example/przemek/astroweather/MainActivity.java
+++ app/src/main/java/com/example/przemek/astroweather/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.przemek.astroweather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.astrocalculator.AstroCalculator;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AstroCalculator astroCalculator;
     }
 }
